3c0d5bbcba259b4f12841e90123ebe33746129e3
luis-luciqm <redbullfc18@gmail.com>
2021-02-18 14:57:54 -0300

    Input de Imagens adicionado

--- pages/index-anunciar-imoveis.php
+++ pages/index-anunciar-imoveis.php
@@ -87,11 +87,18 @@
 
                     <br><p class="text-one-board">Tipo de Imóvel</p>
                     <p class="obrigatorio">*</p><br>
-                   
-                    <input type="text" id="tipo-imovel" class="fadeIn second zero-raduis" name="tipo-imovel" placeholder="Casa, Apartamento, Fazenda, Hotel" required><br>
+                    <input type="text" id="tipo-imovel" class="fadeIn second zero-raduis" name="tipo-imovel" placeholder="Casa, Apartamento, Fazenda, Hotel" required><br><br>
                     
+                    <p class="text-one-board">Preço</p>
+                    <p class="obrigatorio">*</p>
+                    <input type="text" id="preco" class="fadeIn second zero-raduis" name="preco" placeholder="R$ 000.000" required><br><br>
+
+                    <p class="text-one-board">Selecione uma(s) imagem(s) do seu imóvel</p>
+                    <p class="obrigatorio">*</p>
+                    <input type="file" id="inputImagem" name="inputImagem" class="btn btn-sucess" accept="image/png, image/jpeg, image/jpg" required multiple/><br>
+                    <!-- <input type="text" id="imagem" class="fadeIn second zero-raduis" name="imagem" placeholder="R$ 000.000" required><br><br> -->
+
                     <br><p class="text-one-board">Faça uma breve descrição do Imóvel (Opcional)</p>
-                    <!-- <p class="obrigatorio">*</p><br> -->
                     <textarea id="w3review" name="w3review" rows="4" cols="50" placeholder="Ex: Casa com 2 salas, 3 quartos climatizados, próximo ao IFRN Pau do Ferros"></textarea>
                                         
                     <p id="msgErro"></p>
