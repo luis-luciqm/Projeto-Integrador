3c847bbe5ec6b5805857dbc705a81d0752e29426
luis-luciqm <redbullfc18@gmail.com>
2021-02-26 17:53:30 -0300

    Bug Botao Responsivo

--- index.php
+++ index.php
@@ -27,7 +27,7 @@
                 <img class="image-logo" src="images/favicon.png" href="index.php" alt="Imagem da logo">
 
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span>
+                    <span id="menu" class="navbar-toggler-icon">MENU</span>
                 </button>
 
                 <div class="collapse navbar-collapse" id="navbarText">
