55387eef883a64ba935a1809be8eea9e3bf8bca7
luis-luciqm <redbullfc18@gmail.com>
2021-03-02 20:15:11 -0300

    Modal Adicionado

--- index.php
+++ index.php
@@ -146,7 +146,7 @@
 
     <!-- Modal -->
     <div class="modal fade" id="staticBackdrop" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
-    <div class="modal-dialog modal-lg"> <!-- caso não for necessário esse tamanho, retirar, modal-lg -->
+    <div class="modal-dialog modal-lg"> <!-- caso não for necessário esse tamanho, retirar modal-lg -->
         <div class="modal-content">
         <div class="modal-header">
             <h5 class="modal-title" id="staticBackdropLabel">Apartamento</h5>
