a436c3ef917cc9392eb9e34890fc710946f6f733
luis-luciqm <redbullfc18@gmail.com>
2021-03-02 20:16:05 -0300

    Modal Adicionado

--- index.php
+++ index.php
@@ -146,23 +146,23 @@
 
     <!-- Modal -->
     <div class="modal fade" id="staticBackdrop" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
-    <div class="modal-dialog modal-lg"> <!-- caso não for necessário esse tamanho, retirar modal-lg -->
-        <div class="modal-content">
-        <div class="modal-header">
-            <h5 class="modal-title" id="staticBackdropLabel">Apartamento</h5>
-            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+        <div class="modal-dialog modal-lg"> <!-- caso não for necessário esse tamanho, retirar modal-lg -->
+            <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="staticBackdropLabel">Apartamento</h5>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body">
+                <p>Provavelmente uma imagem aqui que será mais viavel</p>
+                <p><b>Cidade: </b>Pau dos Ferros/RN</p>
+                <p>Preço, e afins aqui</p>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                <button type="button" class="btn btn-primary">Understood</button>
+            </div>
+            </div>
         </div>
-        <div class="modal-body">
-            <p>Provavelmente uma imagem aqui que será mais viavel</p>
-            <p><b>Cidade: </b>Pau dos Ferros/RN</p>
-            <p>Preço, e afins aqui</p>
-        </div>
-        <div class="modal-footer">
-            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-            <button type="button" class="btn btn-primary">Understood</button>
-        </div>
-        </div>
-    </div>
     </div>
     <!-- MODAL -->
 
