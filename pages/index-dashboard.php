a80d79e2d0d35337a24b4fe9af35c087d2a04900
luis-luciqm <redbullfc18@gmail.com>
2021-03-03 18:59:05 -0300

    alteraçao em navbar

--- pages/index-dashboard.php
+++ pages/index-dashboard.php
@@ -18,41 +18,37 @@
         $email = $_POST['emailLogin'];
     ?>
 
+    <!-- INICIO NAV-BAR -->
     <header>
         <nav id="navegacao" class="navbar navbar-expand-lg">
             <div class="container-fluid">
                 <!-- <a class="navbar-brand" href="#">Navbar w/ text</a> -->
-                <img class="image-logo" src="../images/favicon.png" href="index.php" alt="Imagem da logo">
+                <img class="image-logo" src="images/favicon.png" href="index.php" alt="Imagem da logo">
 
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span>
+                    <span id="menu" class="navbar-toggler-icon">MENU</span>
                 </button>
 
                 <div class="collapse navbar-collapse" id="navbarText">
                     <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                         <li class="nav-item">
-                            <a class="nav-link" aria-current="page" href="../index.php">Home</a>
+                            <a class="nav-link" aria-current="page" href="../index.php">Início</a>
                         </li>
-                        
                         <li class="nav-item">
-                            <a class="nav-link" href="/pages/index-anunciar-imoveis.php">Anunciar Imóveis</a>
+                            <a class="nav-link" href="index-anunciar-imoveis.php">Anunciar Imóveis</a>
                         </li>
                         <li class="nav-item">
                             <a class="nav-link" href="#">Comprar Imóvel</a>
                         </li>
                     </ul>
 
-                    <?php
-                        if($email == "l.luis@escolar.ifrn.edu.br"){
-                            echo '<b class="navbar-text">LUIS LUCILANDIO DE QUEIROZ MESQUITA</b>';
-                        }else{
-                            echo '<a class="navbar-text" href="pages/index-login.php">Entrar ou Cadastrar</a>';
-                        }
-                    ?>
+                    <a class="navbar-text" href="index-login.php">Entrar ou Cadastrar</a>
+                    <!-- Provavelmente mudar essa tag para button -->
                 </div>
             </div>
         </nav>
     </header>
+    <!-- FIM NAV-BAR -->
 
     <?php
         echo "<h4>Seja bem-vindo, " .$email. "! Organize e visualize seus dados.</h4><br>";
