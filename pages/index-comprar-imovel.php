57f9f3e0749248e5ac00b9b75eef3e3bbdece16e
luis-luciqm <redbullfc18@gmail.com>
2021-03-06 15:01:30 -0300

    ajuste responsividade

--- pages/index-comprar-imovel.php
+++ pages/index-comprar-imovel.php
@@ -65,6 +65,7 @@
                 <ul class="list-group list-group-flush">
                     <li class="list-group-item">Preço: $300,000</li>
                     <li class="list-group-item">Local: Praça da Matriz, Pau dos Ferros - RN</li>
+                    <li class="list-group-item">Tipo: <b>Compra</b></li>
                 </ul>
                 <div class="card-body">
                     <input type="submit" id="botoes-cards" class="fadeIn fourth zero-raduis" value="Ver Imóvel">
