dd30aede28580cfc21f313db7a24b078df215b11
luis-luciqm <redbullfc18@gmail.com>
2021-03-09 22:02:03 -0300

    alinhamento de cards

--- pages/index-comprar-imovel.php
+++ pages/index-comprar-imovel.php
@@ -55,31 +55,34 @@
     <!-- Inicio Imoveis disponiveis para compra -->
     <h4>Imóveis Disponíveis para Compra, e Aluguel.</h4>
 
-    <form class="form-search" method="get" id="s" action="/">
+    <form class="form-search" method="POST" id="s" action="index-comprar-imovel.php">
         <div class="input-append">
             <input type="text" class="input-medium search-query" name="s" placeholder="Pesquisar por Rua, Cidade, Local, etc.." value="">
-            <button class="btn btn-outline-secondary" type="button">Pesquisar</button>
+            <button class="btn btn-outline-secondary" type="submit">Pesquisar</button>
         </div>
     </form>
 
     <div id="cards-compra">
-        <?php for($i = 0; $i < 4; $i++){ ?>
-            <div class="card" data-bs-toggle="modal" data-bs-target="#staticBackdrop">
-                <img src="../images/card1.jpg" class="card-img-top" alt="imagens Pŕoximos a sua Localização">
-                <div class="card-body">
-                    <b><h5 class="card-title">Apartamento</h5></b>
-                    <p class="card-text">Apartamento com 3 quartos, sala de estar, 2 banheiros e cozinha.</p>
+        <p id="trecho2">Resultados da busca de (textoDigitado)</p>
+        <div class="container">
+            <?php for($i = 0; $i < 8; $i++){ ?>
+                <div class="card" data-bs-toggle="modal" data-bs-target="#staticBackdrop">
+                    <img src="../images/card1.jpg" class="card-img-top" alt="imagens Pŕoximos a sua Localização">
+                    <div class="card-body">
+                        <b><h5 class="card-title">Apartamento</h5></b>
+                        <p class="card-text">Apartamento com 3 quartos, sala de estar, 2 banheiros e cozinha.</p>
+                    </div>
+                    <ul class="list-group list-group-flush">
+                        <li class="list-group-item">Preço: $300,000</li>
+                        <li class="list-group-item">Local: Praça da Matriz, Pau dos Ferros - RN</li>
+                        <li class="list-group-item">Tipo: <b>Compra</b></li>
+                    </ul>
+                    <div class="card-body">
+                        <input type="submit" id="botoes-cards" class="fadeIn fourth zero-raduis" value="Ver Imóvel">
+                    </div>
                 </div>
-                <ul class="list-group list-group-flush">
-                    <li class="list-group-item">Preço: $300,000</li>
-                    <li class="list-group-item">Local: Praça da Matriz, Pau dos Ferros - RN</li>
-                    <li class="list-group-item">Tipo: <b>Compra</b></li>
-                </ul>
-                <div class="card-body">
-                    <input type="submit" id="botoes-cards" class="fadeIn fourth zero-raduis" value="Ver Imóvel">
-                </div>
-            </div>
-        <?php } ?>
+            <?php } ?>
+        </div>
     </div>
     <!-- FIM Imoveis disponiveis para compra -->
 
